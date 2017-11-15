7e6e6bdcea87c9e802b93c28a6a90215f94a2a9d
lexiste <todd.fencl+git@gmail.com>
2017-11-15 15:14:47 -0500

    fixed date output, added hostname of computer running script

--- checkHost.sh
+++ checkHost.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+
 if [[ $# -eq 0 ]] ;
  then
    echo "No arguement, pass in host name"
    exit 1
 fi
 
-sNow="$(/bin/date)"
+sNow="$(/bin/date +%d%b%y\ %H:%H:%S)"
 sLook="$(nslookup $1 | grep ^Nam -A1 | awk '{print $2}')"
 sPing="$(ping -c 3 $1 &> /dev/null && echo success || echo fail)"
 
+echo "HOSTNAME: $(/bin/hostname)" >> ~/checkHost.log
 echo "RUN TIME: ${sNow}" >> ~/checkHost.log
 echo "NSLOOKUP: ${sLook}" >> ~/checkHost.log
 echo "PING STATUS: ${sPing}" >> ~/checkHost.log
