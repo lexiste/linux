9af0b60a99d15f0f8727f9b130ac9d64bbcf5bef
lexiste <todd.fencl+git@gmail.com>
2018-03-19 15:26:59 -0400

    new scripts
    added 2 new python scripts to repo

---
+++ host-collector.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import sysconfig
+import argparse
+
+def get_host_ciphers():
+	return
+
+def get_host_screenshot():
+	return
+	
+def main():
+	print "script name: ", sys.argv[0]
+	return
+	
+main()
