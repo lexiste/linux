3b36292f5e5cb73c350a207fb34f3695d821383a
todd fencl <todd.fencl+git@gmail.com>
2018-05-29 13:35:25 -0400

    updated spamhause script to handle checks, download and archive files, etc. just call it from your crontab and make sure you have root perms

--- host-collector.py
+++ host-collector.py
@@ -1,16 +1,41 @@
 #!/usr/bin/python3
 
-import sysconfig
+import sys
 import argparse
 
 def get_host_ciphers():
-	return
+      """
+         to get list of supported ciphers
+      """
+      return
 
 def get_host_screenshot():
-	return
+      """
+         to get list a screen shot from the server
+      """
+      return
 	
 def main():
-	print "script name: ", sys.argv[0]
-	return
+      parser = argparse.ArgumentParser(add_help=True)
+      parser.add_argument('-v', '--verbose', help='increase output verbosity', action='store_true')
+      parser.add_argument('-f', '--file', help='list of IP addresses and port numbers in IP:PORT format')
+      parser.add_argument('--host', help='host IP address to collect')
+      parser.add_argument('--port', help='port number to collect (default:%(default)s)', default=443)
+      args = parser.parse_args()
+
+      if args.verbose:
+         print("verbose mode enabled")
+
+      if args.file:
+         print("file name submitted: ", args.file)
+         input_file = args.file.strip()
+
+      else:
+         host_ip = args.host.strip()
+         port_id = args.port.strip()
+         print("host:",host_ip,":",port_id)
+
+
+      return
 	
 main()
--- spamhaus.sh
+++ spamhaus.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
-#
-# script to load the spamhaus DROP file into iptables firewall rules
-# should not be done less than 12hr, every 24hr is fine for this
-#
-# see for info: https://iplists.firehol.org/?ipset=spamhaus_drop
+##
+## script to load the spamhaus DROP file into iptables firewall rules
+## should not be done less than 12hr, every 24hr is fine for this
+##
+## see for info: https://iplists.firehol.org/?ipset=spamhaus_drop
+##
+## complete replacement for cron job(s) and processing ...
+## just run this script from crontab, no need to check for a file, we'll download
+##  and save to ~/DROP/
+##
 
-if [ -n "$1" ]; then
-   droplist="$1"
-else
-   echo "no file present, provide file as option"
-   exit 1
+folder=~/DROP
+droplist="$folder/spamhaus-drop"
+
+echo "source folder: $folder"
+echo "drop file: $droplist"
+
+if [ ! -d $folder ]; then 
+   echo "drop does not exist!"
+   echo "creating folder"
+   mkdir -p $folder
 fi
+
+if [ -f $droplist ]; then
+   mv $droplist $droplist.$(date +%d%b)
+   echo "move $droplist to $droplist.$(date +%d%b)"
+fi
+
+wget --timeout=20 --quiet -O $droplist https://spamhaus.org/drop/drop.lasso
 
 if [ ! -s "$droplist" ]; then
    echo "unable to find drop list file $droplist " >&2
@@ -24,11 +41,12 @@ if [ ! -x /sbin/iptables ]; then
    exit 1
 fi
 
-# first, delete all rules, delete any chains and reset to "accept all"
-#  this is semi-dangerous since anything other than spamhuas will be reloaded
-#  in a prod world creating a backup, or configuration of other needed (ie. required)
-#  rules would be in another file to be loaded as well
-#  (EX: block telnet in/out, whitelist known hosts, etc.)
+## first, delete all rules, delete any chains and reset to "accept all"
+##  this is semi-dangerous since anything other than spamhuas will be reloaded
+##  in a prod world creating a backup, or configuration of other needed (ie. required)
+##  rules would be in another file to be loaded as well
+##  (EX: block telnet in/out, whitelist known hosts, etc.)
+echo "purging previous iptables rules..."
 /sbin/iptables -P INPUT ACCEPT
 /sbin/iptables -P FORWARD ACCEPT
 /sbin/iptables -P OUTPUT ACCEPT
@@ -37,7 +55,9 @@ fi
 /sbin/iptables -F
 /sbin/iptables -X
 
-# looks like we have the input file and iptables located
+exit 1
+## looks like we have the input file and iptables located
+echo "loading $droplist into iptables..."
 cat "$droplist" \
  | sed -e 's/;.*//' \
  | grep -v '^ *$' \
