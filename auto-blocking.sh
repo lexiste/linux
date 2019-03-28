a7121b3965b9427b17e85b6d669fe4009320e21a
lexiste <todd.fencl+git@gmail.com>
2019-03-28 08:19:23 -0400

    updates to follow best practices

--- auto-blocking.sh
+++ auto-blocking.sh
@@ -10,88 +10,109 @@
 ## just run this script from crontab, no need to check for a file, we'll download
 ##  and save to ~/DROP/
 ##
-## added support for the Team Cymru bogons blocks as well. Bogons blocking 
+## added support for the Team Cymru bogons blocks as well. Bogons blocking
 ##  should only ever be placed at the border router to INBOUND traffic
 ##  (public to private networks)
 ##
+VERSION="0.1"
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename ${__file} .sh)"
 
 folder=~/DROP
 droplist="$folder/spamhaus-drop"
 bogons="$folder/bogons-ipv4"
 
+## redirect stdout/stderr to file
+exec &> $folder/${0##*/}.log
+
 NC='\e[0m' #reset
 ALRT='\e[97m\e[41m' #white fg / red bg
 GOOD='\e[92m' #green fg / no bg (default)
 
-echo -e "source folder: $folder"
-echo -e "drop file: $droplist"
-echo -e "bogon file: $bognons"
+header() {
+  clear
+  echo -e """
+----------------------------------------
+Run Date: ${GOOD}$(date +%d-%b-%Y\ %H:%M)${NC}
+source folder: ${folder}
+drop file: ${droplist}
+bogon file: ${bogons}
+----------------------------------------\n"""
+}
 
-if [ ! -d $folder ]; then 
-   echo -e "drop does not exist!"
-   echo -e "${ALRT}creating folder${NC}"
-   mkdir -p $folder
-fi
+main() {
+  if [ ! -d "${folder}" ]; then
+     echo -e "drop does not exist!"
+     echo -e "${ALRT}creating folder${NC}"
+     mkdir -p ${folder}
+  fi
 
-if [ -f $droplist ]; then
-   mv --force $droplist $droplist.$(date +%d%b)
-   echo -e "move ${GOOD}$droplist${NC} to ${GOOD}$droplist.$(date +%d%b)${NC}"
-fi
-if [ -f $bogons ]; then
-   mv --force $bogons %bogons.$(date +%d%b)
-   echo -e "move ${GOOD}$bogons${NC} to ${GOOD}$bogons.$(date +%d%b)${NC}"
-fi
+  if [ -f "${droplist}" ]; then
+     mv --force ${droplist} ${droplist}.$(date +%d%b)
+     echo -e "move ${GOOD}${droplist}${NC} to ${GOOD}${droplist}.$(date +%d%b)${NC}"
+  fi
 
-wget --timeout=20 --quiet -O $droplist https://spamhaus.org/drop/drop.lasso
-wget --timeout=20 --quiet -O $bogons https://www.team-cymru.org/Services/Bogons/fullbogons-ipv4.txt
+  if [ -f "${bogons}" ]; then
+     mv --force ${bogons} ${bogons}.$(date +%d%b)
+     echo -e "move ${GOOD}${bogons}${NC} to ${GOOD}${bogons}.$(date +%d%b)${NC}"
+  fi
 
-if [ ! -s "$droplist" ]; then
-   echo -e "${ALRT}unable to find drop list file $droplist${NC} " >&2
-   echo -e "perhaps do: wget https://spamhaus.org/drop/drop.lasso -O $droplist"
-   exit 1
-fi
-if [ ! -s "$bogons" ]; then
-   echo -e "${ALRT}unable to find the bogons file $bogons${NC} " >&2
-   echo -e "perhaps a visit to https://www.team-cymru.org/Services/Bogons/fullbogons-ipv4.txt"
-   exit 1
-fi
+  wget --timeout=20 --quiet -O ${droplist} https://spamhaus.org/drop/drop.lasso
+  wget --timeout=20 --quiet -O ${bogons} https://www.team-cymru.org/Services/Bogons/fullbogons-ipv4.txt
 
-if [ ! -x /sbin/iptables ]; then
-   echo -e "${ALRT}missing iptables command line tool, exiting${NC}" >&2
-   exit 1
-fi
+  if [ ! -s "${droplist}" ]; then
+     echo -e "${ALRT}unable to find drop list file ${droplist}${NC} " >&2
+     echo -e "perhaps do: wget https://spamhaus.org/drop/drop.lasso -O ${droplist}"
+     exit 1
+  else
+     # convert the spamhaus file into a Cisco formated ACL file that c/would be used to update router(s)
+     echo -e "converting ${droplist} into ACL format files"
+     cat ${droplist} | grep -v "^;" | awk 'BEGIN {FS=" ; "}; {print $1}' | sed -f ~/scripts/subnet2mask.sed | awk '{ print "deny ip "$1" "$2" any"}' > $droplist.in
+     cat ${droplist} | grep -v "^;" | awk 'BEGIN {FS=" ; "}; {print $1}' | sed -f ~/scripts/subnet2mask.sed | awk '{ print "deny ip any "$1" "$2}' > $droplist.out
+  fi
 
-## first, delete all rules, delete any chains and reset to "accept all"
-##  this is semi-dangerous since anything other than spamhuas will be reloaded
-##  in a prod world creating a backup, or configuration of other needed (ie. required)
-##  rules would be in another file to be loaded as well
-##  (EX: block telnet in/out, whitelist known hosts, etc.)
-echo -e "${GOOD}purging previous iptables rulesi${NC}..."
-/sbin/iptables -P INPUT ACCEPT
-/sbin/iptables -P FORWARD ACCEPT
-/sbin/iptables -P OUTPUT ACCEPT
-/sbin/iptables -t nat -F
-/sbin/iptables -t mangle -F
-/sbin/iptables -F
-/sbin/iptables -X
+  if [ ! -s "${bogons}" ]; then
+     echo -e "${ALRT}unable to find the bogons file $bogons${NC} " >&2
+     echo -e "perhaps a visit to https://www.team-cymru.org/Services/Bogons/fullbogons-ipv4.txt"
+     exit 1
+  else
+     # convert the bogons file into a Cisco formated ACL file that c/would be used to update router(s)
+     echo -e "converting ${bogons} into ACL format files"
+     cat ${bogons} | grep -v "^#" | sed -f ~/scripts/subnet2mask.sed | awk '{ print "deny ip "$1" "$2" any"}' > $bogons.in
+  fi
 
-## looks like we have the input file and iptables located
-echo -e "${GOOD}loading $droplist into iptables${NC}..."
-cat "$droplist" \
- | sed -e 's/;.*//' \
- | grep -v '^ *$' \
- | while read singleBlock ; do
-   /sbin/iptables -I INPUT -s "$singleBlock" -j DROP
-   /sbin/iptables -I OUTPUT -d "$singleBlock" -j DROP
-   /sbin/iptables -I FORWARD -s "$singleBlock" -j DROP
-   /sbin/iptables -I FORWARD -d "$singleBlock" -j DROP
-done
+  if [ ! -x /sbin/iptables ]; then
+     echo -e "${ALRT}missing iptables command line tool, exiting${NC}" >&2
+     exit 1
+  fi
 
-##
-## we have loaded the spamhaus IP's into iptables for local
-## next, let's parse the spamhaus and bogons files into something that could be
-## consumed into Cisco ACL or similiar ... modify as needed
-##
-cat $bogons | grep -v "^#" | sed -f ~/scripts/subnet2mask.sed | awk '{ print "permit ip "$1" "$2" any"}' > bogons.in
-cat $droplist | grep -v "^;" | awk '{BEGIN FS=" ; "}; {print $1}' | sed -f ~/scripts/subnet2mask.sed | awk '{ print "permit ip "$1" "$2" any"}' > spamhaus.in
-cat $droplist | grep -v "^;" | awk '{BEGIN FS=" ; "}; {print $1}' | sed -f ~/scripts/subnet2mask.sed | awk '{ print "permit ip any "$1" "$2}' > spamhaus.out
+  ## first, delete all rules, delete any chains and reset to "accept all"
+  ##  this is semi-dangerous since anything other than spamhuas will be reloaded
+  ##  in a prod world creating a backup, or configuration of other needed (ie. required)
+  ##  rules would be in another file to be loaded as well
+  ##  (EX: block telnet in/out, whitelist known hosts, etc.)
+  echo -e "${ALRT}purging${NC} previous iptables rules..."
+  /sbin/iptables -P INPUT ACCEPT
+  /sbin/iptables -P FORWARD ACCEPT
+  /sbin/iptables -P OUTPUT ACCEPT
+  /sbin/iptables -t nat -F
+  /sbin/iptables -t mangle -F
+  /sbin/iptables -F
+  /sbin/iptables -X
+
+  ## looks like we have the input file and iptables located
+  echo -e "${GOOD}loading${NC} $droplist into iptables..."
+  cat "${droplist}" \
+   | sed -e 's/;.*//' \
+   | grep -v '^ *$' \
+   | while read singleBlock ; do
+     /sbin/iptables -I INPUT -s "$singleBlock" -j DROP
+     /sbin/iptables -I OUTPUT -d "$singleBlock" -j DROP
+     /sbin/iptables -I FORWARD -s "$singleBlock" -j DROP
+     /sbin/iptables -I FORWARD -d "$singleBlock" -j DROP
+  done
+}
+
+header
+main
--- spamhaus.sh
+++ spamhaus.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ##
 ## script to load the spamhaus DROP file into iptables firewall rules
@@ -11,62 +11,83 @@
 ##  and save to ~/DROP/
 ##
 
-folder=~/DROP
-droplist="$folder/spamhaus-drop"
+## variables ... we need them
+VERSION="0.1"
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename ${__file} .sh)"
+__srcFolder=~/DROP
+__droplist="$__srcFolder/spamhaus-drop"
 
+## Colors ... we love them ...
 NC='\e[0m' #reset
 ALRT='\e[97m\e[41m' #white fg / red bg
 GOOD='\e[92m' #green fg / no bg (default)
 
-echo -e "source folder: $folder"
-echo -e "drop file: $droplist"
+header() {
+  clear
+  echo -e """
+----------------------------------------
+  Source Folder: ${__srcFolder}
+  Drop File: ${__droplist}
+----------------------------------------\n"""
+}
 
-if [ ! -d $folder ]; then 
-   echo -e "drop does not exist!"
-   echo -e "${ALRT}creating folder${NC}"
-   mkdir -p $folder
-fi
+main() {
 
-if [ -f $droplist ]; then
-   mv --force $droplist $droplist.$(date +%d%b)
-   echo -e "move ${GOOD}$droplist${NC} to ${GOOD}$droplist.$(date +%d%b)${NC}"
-fi
+  set -o errexit # exit when a command fails
+  set -o nounset # exit when script uses an undeclared variable
 
-wget --timeout=20 --quiet -O $droplist https://spamhaus.org/drop/drop.lasso
+  if [ ! -d ${__srcFolder} ]; then
+     echo -e "drop does not exist!"
+     echo -e "${ALRT}creating Source Folder (${__srcFolder})${NC}"
+     mkdir -p ${__srcFolder}
+  fi
 
-if [ ! -s "$droplist" ]; then
-   echo -e "${ALRT}unable to find drop list file $droplist${NC} " >&2
-   echo -e "perhaps do: wget https://spamhaus.org/drop/drop.lasso -O $droplist"
-   exit 1
-fi
+  if [ -f ${__droplist} ]; then
+     mv --force ${__droplist} ${__droplist}.$(date +%d%b)
+     echo -e "move ${GOOD}${__droplist}${NC} to ${GOOD}${__droplist}.$(date +%d%b)${NC}"
+  fi
 
-if [ ! -x /sbin/iptables ]; then
-   echo -e "${ALRT}missing iptables command line tool, exiting${NC}" >&2
-   exit 1
-fi
+  wget --timeout=20 --quiet -O ${__droplist} https://www.spamhaus.org/drop/drop.lasso
 
-## first, delete all rules, delete any chains and reset to "accept all"
-##  this is semi-dangerous since anything other than spamhuas will be reloaded
-##  in a prod world creating a backup, or configuration of other needed (ie. required)
-##  rules would be in another file to be loaded as well
-##  (EX: block telnet in/out, whitelist known hosts, etc.)
-echo -e "${GOOD}purging${NC} previous iptables rules..."
-/sbin/iptables -P INPUT ACCEPT
-/sbin/iptables -P FORWARD ACCEPT
-/sbin/iptables -P OUTPUT ACCEPT
-/sbin/iptables -t nat -F
-/sbin/iptables -t mangle -F
-/sbin/iptables -F
-/sbin/iptables -X
+  if [ ! -s "${__droplist}" ]; then
+     echo -e "${ALRT}unable to find drop list file ${__droplist}${NC} " >&2
+     echo -e "perhaps do: wget https://www.spamhaus.org/drop/drop.lasso -O ${__droplist}"
+     exit 1
+  fi
 
-## looks like we have the input file and iptables located
-echo -e "${GOOD}loading${NC} $droplist into iptables..."
-cat "$droplist" \
- | sed -e 's/;.*//' \
- | grep -v '^ *$' \
- | while read singleBlock ; do
-   /sbin/iptables -I INPUT -s "$singleBlock" -j DROP
-   /sbin/iptables -I OUTPUT -d "$singleBlock" -j DROP
-   /sbin/iptables -I FORWARD -s "$singleBlock" -j DROP
-   /sbin/iptables -I FORWARD -d "$singleBlock" -j DROP
-done
+  if [ ! -x /sbin/iptables ]; then
+     echo -e "${ALRT}missing iptables command line tool, exiting${NC}" >&2
+     exit 1
+  fi
+
+  ## first, delete all rules, delete any chains and reset to "accept all"
+  ##  this is semi-dangerous since anything other than spamhuas will be reloaded
+  ##  in a prod world creating a backup, or configuration of other needed (ie. required)
+  ##  rules would be in another file to be loaded as well
+  ##  (EX: block telnet in/out, whitelist known hosts, etc.)
+  echo -e "${GOOD}purging${NC} previous iptables rules..."
+  /sbin/iptables -P INPUT ACCEPT
+  /sbin/iptables -P FORWARD ACCEPT
+  /sbin/iptables -P OUTPUT ACCEPT
+  /sbin/iptables -t nat -F
+  /sbin/iptables -t mangle -F
+  /sbin/iptables -F
+  /sbin/iptables -X
+
+  ## looks like we have the input file and iptables located
+  echo -e "${GOOD}loading${NC} ${__droplist} into iptables..."
+  cat "${__droplist}" \
+   | sed -e 's/;.*//' \
+   | grep -v '^ *$' \
+   | while read singleBlock ; do
+     /sbin/iptables -I INPUT -s "$singleBlock" -j DROP
+     /sbin/iptables -I OUTPUT -d "$singleBlock" -j DROP
+     /sbin/iptables -I FORWARD -s "$singleBlock" -j DROP
+     /sbin/iptables -I FORWARD -d "$singleBlock" -j DROP
+  done
+}
+
+header
+main
