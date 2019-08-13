5d66ee0edf9c78ebccdf7b0b029e74c32abfe6d8
lexiste <todd.fencl+git@gmail.com>
2019-08-13 12:56:05 -0400

    improvements and bug fixes
    added talos ioc feed for download as well

--- auto-blocking.sh
+++ auto-blocking.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ##
 ## script to load the spamhaus DROP file into iptables firewall rules
@@ -20,8 +20,9 @@ __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
 __base="$(basename ${__file} .sh)"
 
 folder=~/DROP
-droplist="$folder/spamhaus-drop"
+spamhaus="$folder/spamhaus-drop"
 bogons="$folder/bogons-ipv4"
+talos="$folder/talos-ioc"
 
 ## redirect stdout/stderr to file
 exec &> $folder/${0##*/}.log
@@ -36,54 +37,67 @@ header() {
 ----------------------------------------
 Run Date: ${GOOD}$(date +%d-%b-%Y\ %H:%M)${NC}
 source folder: ${folder}
-drop file: ${droplist}
+spamhaus file: ${spamhaus}
 bogon file: ${bogons}
+talos file: ${talos} [for record keeping]
 ----------------------------------------\n"""
 }
 
 main() {
   if [ ! -d "${folder}" ]; then
-     echo -e "drop does not exist!"
-     echo -e "${ALRT}creating folder${NC}"
+     echo -e "${ALRT}[!!] Creating folder ${folder}${NC}"
      mkdir -p ${folder}
   fi
 
-  if [ -f "${droplist}" ]; then
-     mv --force ${droplist} ${droplist}.$(date +%d%b)
-     echo -e "move ${GOOD}${droplist}${NC} to ${GOOD}${droplist}.$(date +%d%b)${NC}"
+  if [ -f "${spamhaus}" ]; then
+    echo -e "move ${GOOD}${spamhaus}${NC} to ${GOOD}${spamhaus}.$(date +%d%b)${NC}"
+    mv --force ${spamhaus} ${spamhaus}.$(date +%d%b)
   fi
 
   if [ -f "${bogons}" ]; then
-     mv --force ${bogons} ${bogons}.$(date +%d%b)
-     echo -e "move ${GOOD}${bogons}${NC} to ${GOOD}${bogons}.$(date +%d%b)${NC}"
+    echo -e "move ${GOOD}${bogons}${NC} to ${GOOD}${bogons}.$(date +%d%b)${NC}"
+    mv --force ${bogons} ${bogons}.$(date +%d%b)
   fi
 
-  wget --timeout=20 --quiet -O ${droplist} https://spamhaus.org/drop/drop.lasso
-  wget --timeout=20 --quiet -O ${bogons} https://www.team-cymru.org/Services/Bogons/fullbogons-ipv4.txt
+  if [ -f "${talos}" ]; then
+    echo -e "move ${GOOD}${talos}${NC} to ${GOOD}${talos}.$(date +%d%b)${NC}"
+    mv --force ${talos} ${talos}.$(date +%d%b)
+  fi
 
-  if [ ! -s "${droplist}" ]; then
-     echo -e "${ALRT}unable to find drop list file ${droplist}${NC} " >&2
-     echo -e "perhaps do: wget https://spamhaus.org/drop/drop.lasso -O ${droplist}"
+  echo ""
+  wget --timeout=20 --quiet -O ${spamhaus} https://spamhaus.org/drop/drop.lasso
+  echo -e "${GOOD}[+]${NC} downloading ${spamhaus} file"
+  wget --timeout=20 --quiet -O ${bogons} https://www.team-cymru.org/Services/Bogons/fullbogons-ipv4.txt
+  echo -e "${GOOD}[+]${NC} downloading ${bogons} file"
+  wget --timeout=20 --quiet -O ${talos} talos-iocs https://talosintelligence.com/documents/ip-blacklist
+  echo -e "${GOOD}[+]${NC} downloading ${talos} file"
+  echo ""
+
+  if [ ! -s "${spamhaus}" ]; then
+     echo -e "${ALRT}[!!]${NC} unable to find drop list file ${spamhaus}"
+     echo -e "perhaps do: wget https://spamhaus.org/drop/drop.lasso -O ${spamhaus}"
      exit 1
   else
      # convert the spamhaus file into a Cisco formated ACL file that c/would be used to update router(s)
-     echo -e "converting ${droplist} into ACL format files"
-     cat ${droplist} | grep -v "^;" | awk 'BEGIN {FS=" ; "}; {print $1}' | sed -f ~/scripts/subnet2mask.sed | awk '{ print "deny ip "$1" "$2" any"}' > $droplist.in
-     cat ${droplist} | grep -v "^;" | awk 'BEGIN {FS=" ; "}; {print $1}' | sed -f ~/scripts/subnet2mask.sed | awk '{ print "deny ip any "$1" "$2}' > $droplist.out
+     echo -e "converting ${spamhaus} into ACL format files"
+     cat ${spamhaus} | grep -v "^;" | awk 'BEGIN {FS=" ; "}; {print $1}' | sed -f ~/scripts/subnet2mask.sed | awk '{ print "deny ip "$1" "$2" any"}' > $spamhaus.in
+     cat ${spamhaus} | grep -v "^;" | awk 'BEGIN {FS=" ; "}; {print $1}' | sed -f ~/scripts/subnet2mask.sed | awk '{ print "deny ip any "$1" "$2}' > $spamhaus.out
+     echo -e "finished processing ${spamhaus}\n"
   fi
 
   if [ ! -s "${bogons}" ]; then
-     echo -e "${ALRT}unable to find the bogons file $bogons${NC} " >&2
+     echo -e "${ALRT}[!!]${NC} unable to find the bogons file $bogons"
      echo -e "perhaps a visit to https://www.team-cymru.org/Services/Bogons/fullbogons-ipv4.txt"
      exit 1
   else
      # convert the bogons file into a Cisco formated ACL file that c/would be used to update router(s)
      echo -e "converting ${bogons} into ACL format files"
      cat ${bogons} | grep -v "^#" | sed -f ~/scripts/subnet2mask.sed | awk '{ print "deny ip "$1" "$2" any"}' > $bogons.in
+     echo -e "finished processing ${bogons}\n"
   fi
 
   if [ ! -x /sbin/iptables ]; then
-     echo -e "${ALRT}missing iptables command line tool, exiting${NC}" >&2
+     echo -e "${ALRT}[!!]${NC} missing iptables command line tool, exiting"
      exit 1
   fi
 
@@ -92,7 +106,7 @@ main() {
   ##  in a prod world creating a backup, or configuration of other needed (ie. required)
   ##  rules would be in another file to be loaded as well
   ##  (EX: block telnet in/out, whitelist known hosts, etc.)
-  echo -e "${ALRT}purging${NC} previous iptables rules..."
+  echo -e "${ALRT}[!!]${NC} purging previous iptables rules..."
   /sbin/iptables -P INPUT ACCEPT
   /sbin/iptables -P FORWARD ACCEPT
   /sbin/iptables -P OUTPUT ACCEPT
@@ -102,8 +116,8 @@ main() {
   /sbin/iptables -X
 
   ## looks like we have the input file and iptables located
-  echo -e "${GOOD}loading${NC} $droplist into iptables..."
-  cat "${droplist}" \
+  echo -e "${GOOD}loading${NC} $spamhaus into iptables..."
+  cat "${spamhaus}" \
    | sed -e 's/;.*//' \
    | grep -v '^ *$' \
    | while read singleBlock ; do
